fa7c30813f34e004ac8934cda364fea44b9a1f4c
treaptofun <abarankab@gmail.com>
2020-03-01 11:07:43 +0300

    Added pi.cc

--- sum.cc
+++ sum.cc
@@ -9,13 +9,19 @@
 #include "defs.h"
 #include "timer.h"
 
-std::vector<ull> init_data() {
-    const ull MAX_SIZE = 100'000'000;
-    const ull MAX_ELEMENT = 1'000'000;
+std::random_device rd;
+std::mt19937 gen(rd());
 
-    std::vector<ull> data(rand() % MAX_SIZE);
+std::vector<ull> init_data() {
+    static const ull SIZE = 100'000'000;
+    static const ull MAX_ELEMENT = 1'000'000;
+    static std::uniform_int_distribution<ull> distribution(1, MAX_ELEMENT);
+
+    std::vector<ull> data(SIZE);
+
+    #pragma omp parallel for private(gen)
     for (ull& x : data) {
-        x = rand() % MAX_ELEMENT;
+        x = distribution(gen);
     }
     return data;
 }
@@ -24,12 +30,27 @@ ull par_sum(std::vector<ull>& v) {
     omp_set_num_threads(omp_get_max_threads());
 
     ull res = 0;
-    size_t i = 0;
+
+    // This version runs slower because threads stop each other
+    // 
+    // ull local_res = 0; 
+    // #pragma omp parallel private(local_res)
+    // {
+    //     #pragma omp for
+    //     for (i = 0; i < v.size(); ++i) {
+    //         local_res = local_res + v[i];
+    //     }
+    //     #pragma omp critical
+    //     {
+    //         res += local_res;
+    //     }
+    // }
 
     #pragma omp parallel for reduction(+:res)
-    for (i = 0; i < v.size(); ++i) {
+    for (size_t i = 0; i < v.size(); ++i) {
         res += v[i];
     }
+
     return res;
 }
 
@@ -40,7 +61,7 @@ ull seq_sum(std::vector<ull>& v) {
 }
 
 int main() {
-    const size_t NUM_CYCLES = 1'00;
+    const size_t NUM_CYCLES = 10;
     const size_t CYCLE_STOP = NUM_CYCLES / 10;
 
     ull par_time = 0;
@@ -50,26 +71,25 @@ int main() {
 
     for (size_t cycle = 1; cycle <= NUM_CYCLES; ++cycle) {
         std::vector<ull> data = init_data();
-        escape(&data);
-
         ull par_result, seq_result;
 
         {
+            escape(&data);
             ull start_time = currentSeconds();
             par_result = par_sum(data);
             ull end_time = currentSeconds();
             par_time += (end_time - start_time);
+            escape(&par_result);
         }
 
-        { 
+        {
+            escape(&data);
             ull start_time = currentSeconds();
             seq_result = seq_sum(data);
             ull end_time = currentSeconds();
             seq_time += (end_time - start_time);
+            escape(&seq_result);
         }
-
-        escape(&par_result);
-        escape(&seq_result);
 
         if (par_result != seq_result) {
             std::cerr << std::fixed
@@ -90,6 +110,7 @@ int main() {
     std::cout << std::fixed
               << "Final results:\n"
               << "Parallel time: " << par_time / NUM_CYCLES << " ns\n"
-              << "Sequential time: " << seq_time / NUM_CYCLES << " ns\n";
+              << "Sequential time: " << seq_time / NUM_CYCLES << " ns\n"
+              << "Parallelized function is " << (double)seq_time / par_time << " times faster.\n\n";
     return 0;
 }
